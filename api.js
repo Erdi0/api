ede9a327d1c0258b606613168a7b3f8ff8633737
s50312 <s50312@bht-berlin.de>
2023-09-14 02:36:21 +0200

    fixed copy

--- api.js
+++ api.js
@@ -1,18 +1,22 @@
-var url = window.location.href
-if(url.includes("snkrs")) {
-    snkrs_url = url.split('=')[1]
-    window.location = "SNKRS://product/" + snkrs_url; 
+function sleep(milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
-if(url.includes("nike")) {
-    nike_url = url.split('=')[1]
-    window.location = "mynike://x-callback-url/product-details?style-color=" + nike_url + "&redirect=true&platform=ios%60"; 
+
+async function main() {
+    var url = window.location.href
+    if(url.includes("snkrs")) {
+        snkrs_url = url.split('=')[1]
+        window.location = "SNKRS://product/" + snkrs_url; 
+    }
+    if(url.includes("nike")) {
+        nike_url = url.split('=')[1]
+        window.location = "mynike://x-callback-url/product-details?style-color=" + nike_url + "&redirect=true&platform=ios%60"; 
+    }
+    if(url.includes("copy")) {
+        copy_value = url.split('=')[1]
+        await navigator.clipboard.writeText(copy_value)
+        window.location = "discord://discordapp.com/channels/1008083647817072640/1064639476107837521"; 
+    }
 }
-if(url.includes("copy")) {
-    copy_value = url.split('=')[1]
-    var copy_this = document.createElement("input")
-    copy_this.setAttribute("type", "text")
-    copy_this.value = copy_value
-    copy_this.select()
-    copy_this.setSelectionRange(0, 99999)
-    window.location = "discord://discordapp.com/channels/1008083647817072640/1064639476107837521"; 
-}
+
+main()
